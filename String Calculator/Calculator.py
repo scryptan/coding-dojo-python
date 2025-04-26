d3495e9a4e69774d4a77320b179b75ccb606133b
scryptan <m.tre@skbkontur.ru>
2025-04-26 14:59:57 +0500

    base add with comma

---
+++ String Calculator/Calculator.py
@@ -0,0 +1,14 @@
+﻿class Calculator:
+    def test(self):
+        return 'some str'
+
+    def add(self, string: str) -> int:
+        if string == "":
+            return 0
+
+        try:
+            string.index(',')
+        except ValueError:
+            return int(string)
+
+        return sum([int(i.strip()) for i in string.split(',')])
---
+++ String Calculator/CalculatorTest.py
@@ -0,0 +1,18 @@
+﻿import Calculator
+
+calc = Calculator.Calculator()
+
+def test_class_is_working():
+    assert 'some str' == calc.test()
+
+def test_add_empty_returns_0():
+    assert calc.add("") == 0
+
+def test_add_one_number_should_return_it():
+    assert calc.add("1") == 1
+
+def test_add_two_numbers_returns_sum():
+    assert calc.add("1,2") == 3
+
+def test_add_two_numbers_with_space_returns_sum():
+    assert calc.add("1,2 ") == 3
