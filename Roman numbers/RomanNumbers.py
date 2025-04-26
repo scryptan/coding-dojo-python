a5d5e069383e126566d7988f7e856ea1a504cf12
scryptan <m.tre@skbkontur.ru>
2025-04-26 16:32:38 +0500

    roman numbers with simple tests

---
+++ Roman numbers/RomanNumbers.py
@@ -0,0 +1,35 @@
+﻿class RomanNumbers:
+    def __init__(self):
+        self.roman_numerals = {
+            1: 'I',
+            5: 'V',
+            10: 'X',
+            50: 'L',
+            100: 'C',
+            500: 'D',
+            1000: 'M'
+        }
+
+    def int_to_roman(self, num: int) -> str:
+        if not (0 < num < 4000):
+            raise ValueError("Number must be between 1 and 3999")
+
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+
+        roman_numeral = ""
+        for i in range(len(val)):
+            count = num // val[i]
+            roman_numeral += syms[i] * count
+            num -= val[i] * count
+        return roman_numeral
---
+++ Roman numbers/test_RomanNumbers.py
@@ -0,0 +1,38 @@
+﻿import pytest
+
+from RomanNumbers import RomanNumbers
+
+roman_numbers = RomanNumbers()
+
+def test_alive():
+    assert True
+
+def test_int_to_roman_raise_exception_when_negative():
+    with pytest.raises(ValueError) as excinfo:
+        roman_numbers.int_to_roman(-1)
+
+    assert "Number must be between 1 and 3999" == str(excinfo.value)
+
+def test_int_to_roman_raise_exception_when_bigger_than_3999():
+    with pytest.raises(ValueError) as excinfo:
+        roman_numbers.int_to_roman(4000)
+
+    assert "Number must be between 1 and 3999" == str(excinfo.value)
+
+def test_int_to_roman_return_i_when_pass_1():
+    assert 'I' == roman_numbers.int_to_roman(1)
+
+def test_int_to_roman_return_iv_when_pass_4():
+    res = roman_numbers.int_to_roman(4)
+    print(f'\nactual: {res}')
+    assert 'IV' == res
+
+def test_int_to_roman_return_ix_when_pass_9():
+    assert 'IX' == roman_numbers.int_to_roman(9)
+
+def test_int_to_roman_return_xlix_when_pass_49():
+    assert 'XLIX' == roman_numbers.int_to_roman(49)
+
+def test_int_to_roman_return_xxxvii_when_pass_37():
+    assert 'XXXVII' == roman_numbers.int_to_roman(37)
+
